752814187e59b000858af3f0554d4c8e6cae19ee
Andrey Datsenko <bibok-777@mail.ru>
2016-10-05 20:38:11 +0300

    added index controller

---
+++ src/main/java/com/alonmack/IndexController.java
@@ -0,0 +1,16 @@
+package com.alonmack;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/")
+    public String index(Model model) {
+        model.addAttribute("name", "Andrey");
+        return "index";
+    }
+}
+
